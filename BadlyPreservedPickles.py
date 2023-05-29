c3172ef8cff4ecd2090bf82be364f7a97a7be824
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2023-05-29 17:13:01 +0100

    pickles expire properly now

--- BadlyPreservedPickles.py
+++ BadlyPreservedPickles.py
@@ -1,23 +1,33 @@
-import pickle, threading, time
+import pickle, threading, time, os
 
 class BadlyPreservedPickles:
 
     def __init__(self):
         self.fileExpiry = []
+        #clean up existing files
+        for file in os.listdir(os.path.dirname(__file__)):
+            if file.endswith(".pickle"):
+                os.remove(os.path.join(os.path.dirname(__file__), file))
         self.cleanUpThread = threading.Thread(target=self.cleanUp)
-        #self.cleanUpThread.start()
+        self.cleanUpThread.start()
 
     def cleanUp(self):
         while True:
             for file in self.fileExpiry:
                 if file[1] <= time.time():
-                    os.remove(file[0])
+                    path = os.path.join(os.path.dirname(__file__), file[0])
+                    os.remove(path)
+                    print("hi")
                     self.fileExpiry.remove(file)
             time.sleep(1)
 
     def pickle(self, obj, linkName, expiry):
         filename = linkName + ".pickle"
         pickle.dump(obj, open(filename, "wb"))
+        #remove old file if it exists
+        for file in self.fileExpiry:
+            if file[0] == filename:
+                self.fileExpiry.remove(file)
         self.fileExpiry.append((filename, time.time() + expiry))
         return filename
 
