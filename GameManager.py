ab3aa52d14d010311bdd13b5d3042b0c2ee46957
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2023-05-29 03:43:55 +0100

    created templates for gamecreation, as well as a gamemanager class.
    Also created some sample websockets code for the waiting room

---
+++ GameManager.py
@@ -0,0 +1,158 @@
+import random
+import string
+import time
+
+
+class Game:
+    def __init__(self, code, guess_time):
+        self.cards = {}
+        self.usedCards = []
+        self.players = []
+        self.code = code
+        self.rounds = 0
+        self.current_round = 0
+        self.scores = {}
+        self.locked = False
+        self.current_difficulty = 0
+        self.difficulty_votes = {}
+        self.roundStartTime = 0
+        self.drawLeadTime = 5
+        self.drawTime = 60
+        self.guessTime = guess_time
+        self.displayLeaderboardTime = 20
+        self.startVotes = 0
+
+    def add_player(self, player):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        self.players.append(player)
+        self.scores[player] = 0
+        self.locked = False
+
+    def get_card(self):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        if len(self.usedCards) == len(self.cards):
+            self.usedCards = []
+        card = random.choice(self.cards[self.current_difficulty])
+        while card in self.usedCards:
+            card = random.choice(self.cards[self.current_difficulty])
+        self.usedCards.append(card)
+        self.locked = False
+        return card
+
+    def add_score(self, player1, player2, score):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        self.scores[player1] += score
+        self.scores[player2] += score
+        self.locked = False
+
+    def get_leaderboard(self):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        leaderboard = []
+        for player in self.players:
+            leaderboard.append((player, self.scores[player]))
+        leaderboard.sort(key=lambda x: x[1], reverse=True)
+        self.locked = False
+        return leaderboard
+
+    def vote_on_difficulty(self, player, difficulty):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        if player not in self.difficulty_votes:
+            self.difficulty_votes[player] = difficulty
+        self.locked = False
+
+    def set_difficulty(self):
+        # get the most popular vote for difficulty
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        difficulties = {}
+        for player in self.difficulty_votes:
+            if self.difficulty_votes[player] not in difficulties:
+                difficulties[self.difficulty_votes[player]] = 0
+            difficulties[self.difficulty_votes[player]] += 1
+        max_difficulty = 0
+        max_votes = 0
+        for difficulty in difficulties:
+            if difficulties[difficulty] > max_votes:
+                max_votes = difficulties[difficulty]
+                max_difficulty = difficulty
+        self.current_difficulty = max_difficulty
+        self.locked = False
+
+    def start_round(self):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        self.current_round += 1
+        self.set_difficulty()
+        self.roundStartTime = time.time()
+        self.locked = False
+
+    def getCurrentSection(self):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        if time.time() - self.roundStartTime < self.drawLeadTime:
+            self.locked = False
+            return "drawLead"
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime:
+            self.locked = False
+            return "draw"
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime + self.guessTime:
+            self.locked = False
+            return "guess"
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime + self.guessTime + self.displayLeaderboardTime:
+            self.locked = False
+            return "displayLeaderboard"
+        else:
+            self.start_round()
+            self.locked = False
+            return "drawLead"
+
+    def getTimeLeftInSection(self):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        if time.time() - self.roundStartTime < self.drawLeadTime:
+            self.locked = False
+            return self.drawLeadTime - (time.time() - self.roundStartTime)
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime:
+            self.locked = False
+            return self.drawLeadTime + self.drawTime - (time.time() - self.roundStartTime)
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime + self.guessTime:
+            self.locked = False
+            return self.drawLeadTime + self.drawTime + self.guessTime - (time.time() - self.roundStartTime)
+        elif time.time() - self.roundStartTime < self.drawLeadTime + self.drawTime + self.guessTime + self.displayLeaderboardTime:
+            self.locked = False
+            return self.drawLeadTime + self.drawTime + self.guessTime + self.displayLeaderboardTime - (
+                        time.time() - self.roundStartTime)
+        else:
+            self.start_round()
+            self.locked = False
+            return self.drawLeadTime - (time.time() - self.roundStartTime)
+
+    def vote_to_start(self, player):
+        while self.locked:
+            time.sleep(1)
+        self.locked = True
+        self.startVotes += 1
+        if self.startVotes > len(self.players) / 2:
+            self.start_round()
+        self.locked = False
+
+    def getPlayers(self):
+        return self.players
+
+    @staticmethod
+    def generateCode():
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
--- app.py
+++ app.py
@@ -1,12 +1,69 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask_socketio import SocketIO, emit, join_room
 
+from GameManager import Game
 app = Flask(__name__)
-
+app.config['GAMES'] = {}
+#turn debug on
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'your-secret-key'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main_page():  # put application's code here
+    return render_template('home.html')
 
 
+@app.route('/createGame', methods=['GET', 'POST'])
+def create_game():
+    #if post
+    if request.method == 'POST':
+        code = Game.generateCode()
+        while code in app.config['GAMES']:
+            code = Game.generateCode()
+        app.config['GAMES'][code] = Game(code, request.form['guessTime'])
+        app.config['GAMES'][code].add_player(request.form['player'])
+        return code
+    #if get
+    return render_template('createGame.html')
+
+@app.route('/waitingRoom/<code>')
+def waiting_room(code):
+    return render_template('waitingRoom.html', code=code)
+
+@app.route('/joinGame', methods=['GET', 'POST'])
+def join_game():
+    #if post
+    if request.method == 'POST':
+        code = request.form['code']
+        if code not in app.config['GAMES']:
+            return redirect(url_for('bad_code'))
+        app.config['GAMES'][code].add_player(request.form['player'])
+        emit('player_joined', {'player': request.form['player']}, room=code, namespace='/')
+        return code
+    #if get
+    return render_template('joinGame.html')
+
+@app.route('/badCode')
+def bad_code():
+    return render_template('badCode.html')
+@socketio.on('join')
+def handle_join(data):
+    room = data['room']
+    print(room)
+    join_room(room)
+    print('Client joined room:', room)
+    emit('player_joined', {'player': data['player']}, room=data['room'])
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
